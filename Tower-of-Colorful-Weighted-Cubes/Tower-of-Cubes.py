463ddac1d6b954408d307be2ff967314d4dad94d
emad ferdowsi <emad.ferdowsi@outlook.com>
2022-01-20 20:34:45 +0330

    Tower-of-Cubes: Add getSideColor Method
    -Adds getSideColor method to get the color of a given side of the cube

--- Tower-of-Colorful-Weighted-Cubes/Tower-of-Cubes.py
+++ Tower-of-Colorful-Weighted-Cubes/Tower-of-Cubes.py
@@ -6,6 +6,18 @@ class Cube:
         self.W = W
         self.sides = { 0: front , 1: back , 2: left , 3: right , 4: top , 5: bottom }
     
+    
+    def getSideColor(self , side):
+        """get the color of the given side
+
+        Args:
+            side (int): index of the given side
+            
+        Returns:
+            int : color of the given side
+        """
+        return self.sides[side]
+    
     def getOppositeSide(self, side):
         """find the color of the opposite side of the cube
             sides position as { 0: front , 1: back , 2: left , 3: right , 4: top , 5: bottom }
@@ -15,5 +27,4 @@ class Cube:
         Returns:
             int : color of the opposite side
         """
-        return self.sides[(side+1) if (side%2==0) else side - 1]
-        
+        return self.sides[(side+1) if (side%2==0) else side - 1]        
