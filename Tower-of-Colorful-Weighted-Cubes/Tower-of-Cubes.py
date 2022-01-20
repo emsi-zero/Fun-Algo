0a742568d62fc1d4a56229216e149c4915e65d1f
emad ferdowsi <emad.ferdowsi@outlook.com>
2022-01-20 19:52:28 +0330

    Tower-of-Cubes: Initiate Code and Cube class
    -Initiate Cube Class
    -Add init method(constructor)

---
+++ Tower-of-Colorful-Weighted-Cubes/Tower-of-Cubes.py
@@ -0,0 +1,13 @@
+
+
+class Cube:
+    def __init__(self, W , front, back , left , right , top , bottom) -> None:
+        self.W = W
+        self.front = front
+        self.back = back
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+    
+    pass
