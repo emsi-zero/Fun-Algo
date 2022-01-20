6dec5b84d4ae8cd86cb338ac6872dc327d0d2311
emad ferdowsi <emad.ferdowsi@outlook.com>
2022-01-20 20:23:52 +0330

    Tower-of-Cubes: Add getOppositeSide method  -Adds getOppositeSide method to Cube class to get the color of the opposite side of the cube  -Changes the constructor so that sides dictionaru replaces side-fields

--- Tower-of-Colorful-Weighted-Cubes/Tower-of-Cubes.py
+++ Tower-of-Colorful-Weighted-Cubes/Tower-of-Cubes.py
@@ -1,13 +1,22 @@
 
 
+
+from typing_extensions import Self
+
+
 class Cube:
     def __init__(self, W , front, back , left , right , top , bottom) -> None:
         self.W = W
-        self.front = front
-        self.back = back
-        self.left = left
-        self.right = right
-        self.top = top
-        self.bottom = bottom
+        self.sides = { 0: front , 1: back , 2: left , 3: right , 4: top , 5: bottom }
     
-    pass
+    def getOppositeSide(self, side):
+        """find the color of the opposite side of the cube
+            sides position as { 0: front , 1: back , 2: left , 3: right , 4: top , 5: bottom }
+        Args:
+            side (int): given side of the cube
+        
+        Returns:
+            int : color of the opposite side
+        """
+        return self.sides[(side+1) if (side%2==0) else side - 1]
+        
