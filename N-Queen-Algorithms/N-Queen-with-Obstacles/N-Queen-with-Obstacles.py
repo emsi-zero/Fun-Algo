f3daa35508833d37eab035896f831d400fc862c1
emad ferdowsi <emad.ferdowsi@outlook.com>
2022-01-21 23:02:31 +0330

    N-Queen-Obstacles: Update Scan function
    -Update ScanDanger function to scan 3 directions above given queen

--- N-Queen-Algorithms/N-Queen-with-Obstacles/N-Queen-with-Obstacles.py
+++ N-Queen-Algorithms/N-Queen-with-Obstacles/N-Queen-with-Obstacles.py
@@ -1,4 +1,44 @@
 
 
-def ScanDanger(board , row , col):
-    pass
+def ScanDanger(n,board , row , col):
+    """Can the board for attacking queens
+
+    Args:
+        n (int): board size
+        board (list): chessboard
+        row (int): row of the given queen
+        col (int): column of the given queen
+    
+    Returns:
+        boolean: false if safe, true if not
+    """
+    
+    # top-left direction
+    i = row
+    j = col
+    while(i>=0 and j>=0):
+        if(board[i][j] == "Q"):
+            return True
+        if(board[i][j] == "W"):
+            break
+        i-= 1
+        j-= 1
+    
+    #top-right
+    while(i>= 0 and j < n):
+        if(board[i][j] == "Q"):
+            return True
+        if(board[i][j]== "W"):
+            break
+        i-= 1
+        j+= 1   
+    
+    # top
+    while(i>=0):
+        if(board[i][j] == "Q"):
+            return True
+        if(board[i][j]== "W"):
+            break
+        i-= 1
+        
+    return False
