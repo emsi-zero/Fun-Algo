6ad71d8e5c7f4051af70bf087711e3be87eb3935
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-02-03 22:35:19 +0330

    Check Danger
    -Adds ScanDanger function to check for dangers of a placement

--- N-Queen-Algorithms/N-Queen/N-Queen.py
+++ N-Queen-Algorithms/N-Queen/N-Queen.py
@@ -2,11 +2,45 @@
 
 solutions =[]           # All possible solutions of N Queen Problem
 n = 0                   # Dimesion of the board
-board = [[]]            # n*n Board
+board = [[" " * n] *n]            # n*n Board
 
 
 def ScanDanger(row ,col):
-    pass
+    """Checks for dangers of attacking queens 
+
+    Args:
+        row (int): row number
+        col (int): col number
+
+    Returns:
+        boolean: danger of attacking queens
+    """
+    # top-left direction
+    i = row
+    j = col
+    while(i>=0 and j>=0):
+        if board[i][j] == "Q":
+            return True
+        i-= 1
+        j-= 1
+    
+    # top direction
+    i = row
+    while(i>=0):
+        if board[i][col] == "Q":
+            return True
+        i-=1
+    
+    # top-right direction
+    i = row
+    j = col
+    while( i>=0 and j<n):
+        if board[i][j] == "Q":
+            return True
+        i-=1
+        j+=1
+    
+    return False
 
 def PlaceQ(row):
     pass
