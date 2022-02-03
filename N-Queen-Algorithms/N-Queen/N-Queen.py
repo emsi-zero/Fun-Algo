781883c1a133c1fa6844753fae3b174020e65955
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2022-02-03 22:11:40 +0330

    Initiate N-Queen Problem
    -Adds board,dimension and solutions
    -Adds function defenitions

---
+++ N-Queen-Algorithms/N-Queen/N-Queen.py
@@ -0,0 +1,22 @@
+
+
+solutions =[]           # All possible solutions of N Queen Problem
+n = 0                   # Dimesion of the board
+board = [[]]            # n*n Board
+
+
+def ScanDanger(row ,col):
+    pass
+
+def PlaceQ(row):
+    pass
+
+def printBoard():
+    pass
+
+def clearBoard():
+    pass
+
+def printSolutions():
+    pass
+
