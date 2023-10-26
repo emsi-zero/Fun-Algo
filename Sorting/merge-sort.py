97a9ab4466391ade12aab37917d85f71c6c72ca7
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2023-10-26 21:46:08 +0330

    reformat merge sort code

--- Sorting/merge-sort.py
+++ Sorting/merge-sort.py
@@ -1,55 +1,50 @@
-
-
-
 import math
 
 
-def divide(list):
-    l = math.ceil(len(list) /2)
-    return (list[:l], list[l:])
+def divide(unsorted_list):
+    l = math.ceil(len(unsorted_list) / 2)
+    return unsorted_list[:l], unsorted_list[l:]
 
 
 def merge(left, right):
-    l, r = 0 , 0
+    l, r = 0, 0
     merged = []
     for h in range(len(left) + len(right)):
-        
+
         if l == len(left):
             merged.extend(right[r:])
             break
         elif r == len(right):
             merged.extend(left[l:])
             break
-        
+
         if left[l] == right[r]:
             merged.append(left[l])
-            l+=1
+            l += 1
             merged.append(right[r])
-            r+=1
+            r += 1
         elif left[l] > right[r]:
             merged.append(right[r])
-            r+=1
+            r += 1
         else:
             merged.append(left[l])
-            l+=1
-        h+= 1
+            l += 1
+        h += 1
     print(merged)
     return merged
 
-def merge_sort(list):
-    if len(list) == 1:
-        return list
-    divided = divide(list)
-    print(divided[0],divided[1])
-    
-    
+
+def merge_sort(unsorted_list):
+    if len(unsorted_list) == 1:
+        return unsorted_list
+    divided = divide(unsorted_list)
+    print(divided[0], divided[1])
+
     sorted_left = merge_sort(divided[0])
     sorted_right = merge_sort(divided[1])
-    
+
     return merge(sorted_left, sorted_right)
-    
-    
 
 
-list = [5,1,4,2,3]
-print(merge_sort(list))
+input_list = [5, 1, 4, 2, 3]
+print(merge_sort(input_list))
